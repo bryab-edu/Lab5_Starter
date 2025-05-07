8d2d123dcb0ebcc2f72f6d389e50b38855f3981a
bryab-edu <156254217+bryab-edu@users.noreply.github.com>
2025-05-07 00:29:11 -0700

    implement all scripts for explore.js and address Unit test q

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 Bryan Canonizado
+1. No, I wouldn't use a JUnit test for testing the whole "message" feature as the cons in the markup describe how individual components can't interact with each other and testing the whole feature seems to involve a lot more than simple logical issues (i.e validation).
+2. Yes, max length seems like a logistics issue that can be tested more "simply"
+
+
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,62 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  let voiceSel = document.getElementById("voice-select");
+  
+  if (typeof speechSynthesis === "undefined") {
+    alert("your browser does not have built in voices!");
+    return;
+  }
+
+  let speech = window.speechSynthesis;
+  let voices = speech.getVoices();
+
+  speech.addEventListener("voiceschanged", () => {
+  voices = speech.getVoices();
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSel.appendChild(option);
+    }
+  });
+
+  let textRead = document.querySelector("textarea");
+  let button = document.querySelector("button");
+
+  button.addEventListener("click", function(){
+    let utter = new SpeechSynthesisUtterance(textRead.value);
+    let selected = voiceSel.selectedOptions[0].getAttribute("data-name");
+    console.log(selected);
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selected) {
+        utter.voice = voices[i];
+      }
+    }
+
+    let face = document.querySelector("img")
+    utter.onstart = function () {
+      face.src = "assets/images/smiling-open.png";
+    };
+    utter.onend = function () {
+      face.src = "assets/images/smiling.png";
+    };
+
+
+    speech.speak(utter);
+    textRead.blur();
+  })
+
+  // speech.speaking.addEventListener("change", function(){
+  //   let face = document.querySelector("img")
+  //   if(speech.speaking){
+  //     face.src = "assets/images/smiling-open.svg";
+  //   }
+  //   else{
+  //     face.src = "assets/images/smiling.svg";
+  //   }
+  // })
+
 }
